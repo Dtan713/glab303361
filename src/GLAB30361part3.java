88e3412616b112ceef0295a9116a16529caaadb1
Dtan713 <s.donaldtan@gmail.com>
2024-07-01 12:39:43 -0400

    adding part 3

--- src/GLAB30361part3.java
+++ src/GLAB30361part3.java
@@ -1,2 +1,9 @@
 public class GLAB30361part3 {
+    public static void main(String[] args) {
+        String[] names = {"New York", "Dallas", "Las Vegas", "Florida"};
+         for (String name : names) {
+             System.out.println(name);
+         }
+
+    }
 }
